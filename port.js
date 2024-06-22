0c8290c302857e47653bfce6ea1aa528c485a212
wallingm <jamesmwalling@gmail.com>
2024-06-22 16:49:49 -0400

    - finalize resume - animate homepage text - add mobile menu - other bug fixes

--- port.js
+++ port.js
@@ -1,19 +1,62 @@
-var darkMode = sessionStorage.getItem('darkMode') || false;
-darkMode = (darkMode == 'true');
-var darkModeFirst = darkMode;
-
 const bgPrimary = '#1ee384';
 const hash = document.location.hash;
 const showBgAnimation = (hash == '');
 
+var darkMode = false;
+const navElem = $('#nav');
 const spotlight = $('#spotlight');
 
+// typewriter effect
+const typewriterElems = $('.typewriter');
+const typewriterInitDelay = 7500; // delay in ms before initializing
+const typewriterSpeed = 36; // speed in ms between each character
+const typewriterPause = 300; // pause in ms between each element
+const homeIconBottom = $('section#home .icon-bottom');
+function typewriterInit() {
+    let currentElem = 0;
+    let charIndex = 0;
+
+    // Function to write text in typewriter effect
+    function typewriterWrite() {
+        let element = typewriterElems.eq(currentElem);
+        let text = element.data('text');
+
+        if (charIndex < text.length) {
+            element.append(text.charAt(charIndex));
+            element.addClass('active').removeClass('waiting');
+            charIndex++;
+            setTimeout(typewriterWrite, typewriterSpeed);
+        } else {
+            // Move to the next element after a pause
+            element.removeClass('active');
+            charIndex = 0;
+            currentElem++;
+            if (currentElem < typewriterElems.length) {
+                setTimeout(typewriterWrite, typewriterPause);
+            } else {
+                homeIconBottom.slideDown();
+            }
+        }
+    }
+
+    // Start the typewriter effect
+    typewriterWrite();
+}
 $(document).ready(function() {
-    
-    createTimeline(); // create html for project section timeline
-
-    checkDarkMode();
-
+    if (showBgAnimation) {
+        // store and clear the elems text content
+        homeIconBottom.hide();
+        typewriterElems.each(function(index) {
+            let text = $(this).text();
+            $(this).data('text', text);
+            $(this).empty();
+            if (index == 0) {
+                $(this).addClass('active waiting');
+            }
+        });
+        // initialize after delay
+        setTimeout(typewriterInit, typewriterInitDelay);
+    }
 });
 
 
@@ -36,6 +79,7 @@ function createTimeline() {
     });
     updateTimeline();
 }
+
 function updateTimeline() {
     if ($('.timeline-group:not(.active-timeout)').length > 0) {
         let windowHeight = $(window).height();
@@ -56,7 +100,19 @@ function updateTimeline() {
         });
     }
 }
-$(window).on('scroll', updateTimeline);
+$(document).ready(createTimeline); // create html for project section timeline
+
+$(window).on('scroll', function() {
+    // update nav backdrop
+    let windowScrollTop = $(window).scrollTop();
+    if (windowScrollTop > 100 && !darkMode) {
+        navElem.addClass('backdrop');
+    } else {
+        navElem.removeClass('backdrop');
+    }
+
+    updateTimeline();
+});
 
 // contact me
 $('#contactMe').on('click', function() {
@@ -67,38 +123,58 @@ $('#contactMe').on('click', function() {
 });
 
 
+// mobile menu
+function toggleMobileMenu() {
+    $('#nav .nav-right').toggleClass('display-mobile-menu');
+}
+$('#toggle-mobile-menu').on('click', toggleMobileMenu);
+$(document).on('click', '.nav-right.display-mobile-menu a', toggleMobileMenu);
+
 // dark mode
-function checkDarkMode(toggle = false) {
-    if (toggle) {
-        darkMode = !darkMode;
-        sessionStorage.setItem('darkMode', darkMode);
-
-        if (darkModeFirst && !darkMode) { location.reload(); }
-    }
-
+function toggleDarkMode() {
+    darkMode = !darkMode;
     if (darkMode) {
         $('body').addClass('dark-mode');
-        $('#toggleDarkMode > span').text('Light Mode');
-        $('#spotlight').fadeIn();
+        $('#toggle-dark-mode').attr('title','Light Mode');
+        $('#toggle-dark-mode i').removeClass('fa-moon').addClass('fa-sun');
+        spotlight.fadeIn();
+        spotlight.css('pointer-events', (pointerIsCoarse()) ? 'auto' : 'none');
         $(document).on('mousemove click', mouseMoveSpotlight);
 
     } else {
         $('body').removeClass('dark-mode');
-        $('#toggleDarkMode > span').text('Dark Mode');
-        $('#spotlight').fadeOut();
+        $('#toggle-dark-mode').attr('title','Dark Mode');
+        $('#toggle-dark-mode i').removeClass('fa-sun').addClass('fa-moon');
+        spotlight.fadeOut();
         $(document).off('mousemove click', mouseMoveSpotlight);
 
     }
 }
-$('#toggleDarkMode').on('click', function() {
-    checkDarkMode(true);
-});
+$('#toggle-dark-mode').on('click', toggleDarkMode);
+
+let spotlightClickTimeout;
+let spotlightClickResetTimeout;
 function mouseMoveSpotlight(e) {
     let positionLeft = e.clientX - spotlight.width()/2;
     let positionTop = e.clientY - spotlight.height()/2;
-    spotlight.css({'left': positionLeft, 'top': positionTop});
-}
 
+    spotlight.css({'left': positionLeft, 'top': positionTop});
+
+    // if mobile, only allow double clicks when using spotlight
+    if (pointerIsCoarse()) {
+        clearTimeout(spotlightClickTimeout);
+        clearTimeout(spotlightClickResetTimeout);
+        spotlightClickTimeout = setTimeout(function() {
+            spotlight.css('pointer-events','none');
+        }, 50);
+        spotlightClickResetTimeout = setTimeout(function() {
+            spotlight.css('pointer-events','auto');
+        }, 600);
+    }
+}
+function pointerIsCoarse() {
+    return (window.matchMedia("(pointer: coarse)").matches);
+}
 
 // background animation
 function skipBgAnimation() {
