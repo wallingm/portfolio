628c1f97b74fa5f418573bdd08eafa8c504dc6f4
wallingm <jamesmwalling@gmail.com>
2024-06-28 17:32:08 -0400

    - add more project videos - video fullscreen ability - small resume changes

--- port.js
+++ port.js
@@ -57,6 +57,34 @@ $(document).ready(function() {
         // initialize after delay
         setTimeout(typewriterInit, typewriterInitDelay);
     }
+
+    // toggle video fullscreen
+    $('.timeline-content-video video').on('click', function() {
+        let video = $(this).get(0);
+        
+        if (!document.fullscreenElement) {
+            if (video.requestFullscreen) {
+                video.requestFullscreen();
+            } else if (video.mozRequestFullScreen) { // Firefox
+                video.mozRequestFullScreen();
+            } else if (video.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                video.webkitRequestFullscreen();
+            } else if (video.msRequestFullscreen) { // IE/Edge
+                video.msRequestFullscreen();
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) { // Firefox
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+            }
+        }
+    });
+
 });
 
 
